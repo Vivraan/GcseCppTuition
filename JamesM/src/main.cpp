0a0b402d30e3416748589e26353f94cc5810e1fc
Vivraan-KU <k2169461@kingston.ac.uk>
2023-07-26 15:04:21 +0100

    Update classwork

--- JamesM/src/main.cpp
+++ JamesM/src/main.cpp
@@ -1,73 +1,60 @@
 // GcseCppTuition.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <cassert>
-#include <cstdint>
+#include <ctime>
 #include <iostream>
+#include <random>
 #include <string>
+#include <unordered_set>
+#include <vector>
 
-// EXTRA CREDIT: What does constexpr mean? Why is it useful?
-constexpr char not_implemented[] = "Not implemented";
+// REVISE: What is the style of naming I am following throughout this code?
+// EXTRA CREDIT: Can you identify which major software foundation writes code like this? Hint: It's not Google.
+void play_loony_lips() {
+	const std::unordered_set<std::string_view> placeholder_set{"{PLACE}", "{ANIMAL}", "{FOOD}", "{PROFESSIONAL}", "{COLOR}", "{RUNNING_VERB}", "{EATING_VERB}", "{COLOR_ADJECTIVE}", "{FEELING_ADJECTIVE}"};
+	const std::vector placeholders(begin(placeholder_set), end(placeholder_set));
+	// QUESTION 1: What did I just do?
 
-// HOMEWORK 1: Write a function that takes a string and returns the number of vowels and consonants in it.
-void count_vowels_and_consonants(const char input[], uint16_t& vowels, uint16_t& consonants) {
-	throw std::exception(not_implemented);
-	vowels = 0;
-	consonants = 0;
+	const std::vector<std::string_view> stories = {
+		"{PROFESSIONAL} went for a {RUNNING_VERB} to the {PLACE} with a {COLOR_ADJECTIVE} {ANIMAL}.",
+		"In {PLACE}, a {FEELING_ADJECTIVE} {ANIMAL} saw the {PROFESSIONAL} {EATING_VERB} {FOOD}.",
+		"The {PROFESSIONAL} loved to {EATING_VERB} {COLOR} {FOOD} at the {FEELING_ADJECTIVE} {PLACE}.",
+		"While {RUNNING_VERB}, the {COLOR_ADJECTIVE} {ANIMAL} dropped the {FOOD} picked up by the {PROFESSIONAL}."};
+
+	std::default_random_engine random_engine{static_cast<uint32_t>(std::time(nullptr))};// NOLINT(cert-msc51-cpp)
+	// CONSIDER: Why would I use std::time above?
+	std::uniform_int_distribution<size_t> dist{0, stories.size() - 1};
+
+	// ReSharper disable once CppLocalVariableMayBeConst
+	std::string story{stories[dist(random_engine)]};
+
+	/*******************************************************
+	 * <CODE STARTS HERE>
+	 * Put your code below this comment block
+	 *******************************************************/
+
+	// TODO! Replace all placeholders in the story with user input
+	// Don't worry, this is not a test, I'll help you write this code.
+
+	/*******************************************************
+	 * <CODE ENDS HERE>
+	 * Put your code above this comment block
+	 *******************************************************/
+
+	std::cout << "\nHere's your story: \n"
+						<< story << "\n";
 }
-// CONSIDER: What happens if you use pointers (int* a) instead of references (int& a)?
-// EXTRA CREDIT: How will you return multiple values without using '&' or '*'?
-
-// HOMEWORK 2: Let's get funky! Write a function that takes a string and returns a string with the vowels removed.
-// Hint: you may want to use std::string for this.
-std::string remove_vowels(const std::string& input) {
-	throw std::exception(not_implemented);
-	return "";
-}
-// CONSIDER: Use std::string::erase() to remove characters from a string.
-// EXTRA CREDIT: Do this using a C-style string (char*) by allocating only 10% extra space at a time.
-
-// CONSIDER HOMEWORK 3: Write a function which only returns characters at indices which are part of the Fibonacci sequence.
-// Hint: The Fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21, ... (each number is the sum of the previous two).
-// Example: "Hello World" -> "Heell r"; "Dinosaurs" -> "Diinas"; "Fortnite" -> "Foorti"; "James" -> "Jaame"
-std::string fibonacci_characters(const std::string& input) {
-	throw std::exception(not_implemented);
-	return "";
-}
-
-// EXTRA CREDIT: Write HOMEWORK 1, 2, and 3 without using std::string OR char[] OR std::vector OR std::array OR pointers (directly).
 
 int main() {
-	const auto str = "Hello World";
-	// QUESTION: What's the type of "Hello World"?
-
-	const std::string no_vowels = remove_vowels(str);
-	assert(no_vowels == "Hll Wrld");
-	// QUESTION: What is assert() doing here?
-
-	uint16_t vowels, consonants;
-	// REVISE: Change the type of these variables so that the meaning of the program remains exactly the same.
-	count_vowels_and_consonants(no_vowels.c_str(), vowels, consonants);
-	// QUESTION: Why does this work?
-
-	assert(vowels == 0 && consonants == no_vowels.size() - 1);
-
-	try {
-		// Finale...
-		const std::string fib_chars = fibonacci_characters(no_vowels);
-		const std::remove_reference_t<decltype(fib_chars[0])> answer[] = {72, 108, 108, 108, 32, 114, 0};
-		// EXTRA CREDIT: What is the type of answer? Hint: it's a trick question! Notice anything peculiar about the array?
-		assert(fib_chars == answer);
-		std::cout << "Result: \"" << fib_chars << "\"\nAll tests passed!\n";
-	} catch (const std::exception& e) {
-		if (e.what() == std::string{not_implemented}) {
-			std::cout << "All tests passed!\n";
-		} else {
-			std::cerr << "Error: " << e.what() << '\n';
-		}
-	}
-
-	// REVISE: Give me an example of an escape character that does not get printed on the console but does something unique.
+	char choice;
+	do {
+		play_loony_lips();
+		std::cout << "Do you want to play again? (y/n): ";
+		std::cin >> choice;
+		std::cin.ignore();// REVISE: What's this doing here?
+		std::cout << "\n";
+	} while (choice == 'y' || choice == 'Y');
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
